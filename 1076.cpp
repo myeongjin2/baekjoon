de7981caae528c72349b18c736eeeb1c89fbf994
myeongjin2 <myeongjin0@naver.com>
2017-08-14 22:28:44 +0900

    Create 1076.cpp

---
+++ 1076.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main() {
+	map<string, int> d = { {"black", 0 }, {"brown",1}, {"red", 2},{"orange", 3}, {"yellow", 4}, {"green", 5},{"blue", 6},{"violet", 7}, {"grey", 8},{"white", 9} };
+
+	string a, b, c;
+	cin >> a >> b >> c;
+
+	long long ans = (long long)(d[a] * 10 + d[b]);
+	for (int k = 0; k < d[c]; k++) {
+		ans *= 10LL;
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
