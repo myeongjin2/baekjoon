97304eaac6dfb74f1e5eecd826cf9cd1724c61ec
myeongjin2 <myeongjin0@naver.com>
2017-08-29 23:00:25 +0900

    Create 11279.java

---
+++ 11279.java
@@ -0,0 +1,33 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	static class Compare implements Comparator<Integer>{
+			public int compare(Integer one, Integer two) {
+				return two.compareTo(one);
+			}
+		
+	}
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		Compare cmp = new Compare();
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>(1,cmp);
+		int n = sc.nextInt();
+		while(n-->0) {
+			int x = sc.nextInt();
+			if(x==0) {
+				if(q.isEmpty()) {
+					System.out.println(0);
+				}
+				else {
+					System.out.println(q.poll());
+				}
+			}
+			else {
+				q.offer(x);
+			}
+		}
+	}
+}
