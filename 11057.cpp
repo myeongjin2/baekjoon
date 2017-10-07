c988ba5d1bef4ea33bc37461cbcb2bd58844efc0
myeongjin2 <myeongjin0@naver.com>
2017-10-07 14:08:41 +0900

    Create 11057.cpp

---
+++ 11057.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long d[1001][10] = {0,};
+	long long mod = 10007;
+	int n;
+	cin >> n;
+	for (int i = 0; i <= 9; i++) {
+		d[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = 0; k <= j; k++) {
+				d[i][j] += d[i - 1][k];
+				d[i][j] %= mod;
+			}
+		}
+	}
+	long long ans = 0;
+	for (int i = 0; i<10; i++) {
+		ans += d[n][i];
+	}
+	ans %= mod;
+	cout << ans << '\n';
+	return 0;
+}
