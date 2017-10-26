0d4d59cc33de665934afcf2267b890cf383e4ef1
myeongjin2 <myeongjin0@naver.com>
2017-10-26 15:21:00 +0900

    Update 1707.cpp

--- 1707.cpp
+++ 1707.cpp
@@ -4,50 +4,48 @@
 #include <vector>
 #include <queue>
 using namespace std;
-
 vector<int> a[20001];
 int color[20001];
-
 void dfs(int node, int c) {
-	int i = 0, next;
-	color[node] = c;
-	for (i = 0; i < a[node].size(); i++) {
-		next = a[node][i];
-		if (color[next] == 0) {
-			dfs(next, 3 - c);
-		}
-	}
+    color[node] = c;
+    for (int i=0; i<a[node].size(); i++) {
+        int next = a[node][i];
+        if (color[next] == 0) {
+            dfs(next, 3-c);
+        }
+    }
 }
-
 int main() {
-	int t, n, m, i = 0, u, v, k = 0, j = 0;
-	scanf("%d", &t);
-	while (t--) {
-		scanf("%d%d", &n, &m);
-		for (i = 1; i <= n; i++) {
-			a[i].clear();
-			color[i] = 0;
-		}
-		for (i = 0; i < m; i++) {
-			scanf("%d%d", &u, &v);
-			a[u].push_back(v);
-			a[v].push_back(u);
-		}
-		for (i = 1; i <= n; i++) {
-			if (color[i] == 0) {
-				dfs(i, 1);
-			}
-		}
-		bool ok = true;
-		for (i = 1; i <= n; i++) {
-			for (k = 0; i < a[i].size(); k++) {
-				j = a[i][k];
-				if (color[i] == color[j]) {
-					ok = false;
-				}
-			}
-		}
-		printf("%s\n", ok ? "YES" : "NO");
-	}
-	return 0;
+    int t;
+    scanf("%d\n",&t);
+    while (t--) {
+        int n, m;
+        scanf("%d %d",&n,&m);
+        for (int i=1; i<=n; i++) {
+            a[i].clear();
+            color[i] = 0;
+        }
+        for (int i=0; i<m; i++) {
+            int u,v;
+            scanf("%d %d",&u,&v);
+            a[u].push_back(v);
+            a[v].push_back(u);
+        }
+        for (int i=1; i<=n; i++) {
+            if (color[i] == 0) {
+                dfs(i, 1);
+            }
+        }
+        bool ok = true;
+        for (int i=1; i<=n; i++) {
+            for (int k=0; k<a[i].size(); k++) {
+            int j = a[i][k];
+                if (color[i] == color[j]) {
+                    ok = false;
+                }
+            }
+        }
+        printf("%s\n",ok ? "YES" : "NO");
+    }
+    return 0;
 }
